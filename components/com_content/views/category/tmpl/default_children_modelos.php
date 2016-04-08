9830cf5b9cce76f82ee0d6a3da838a8306969912
Mariana Lino Ferreira <marianas2lino@gmail.com>
2016-04-08 12:43:11 -0300

    Verificacao e criacao de modelo para categoria de celulares - modelos. Nao utilizado no momento, apenas teste

--- components/com_content/views/category/tmpl/default_children_modelos.php
+++ components/com_content/views/category/tmpl/default_children_modelos.php
@@ -11,20 +11,19 @@ defined('_JEXEC') or die;
 
 JHtml::_('bootstrap.tooltip');
 
-	$class = ' class="first"';
+	$class = ' class="uk-width-medium-1-5 cellphone first"';
 	$lang  = JFactory::getLanguage();
 	
 	if(count($this->children[$this->category->id]) > 0):
 		foreach ($this->children[$this->category->id] as $id => $child):
 			if ($this->params->get('show_empty_categories') || $child->getNumItems(true) || count($child->getChildren())):
 				if (!isset($this->children[$this->category->id][$id + 1])):
-					$class = ' class="last"';
+					$class = ' class="uk-width-medium-1-5 cellphone last"';
 				endif;
 ?>
-
 				<div<?php echo $class; ?>>
 <?php
-					$class = '';
+					$class = ' class="uk-width-medium-1-5 cellphone"';
 					if ($lang->isRtl()):
 ?>
 						<h3 class="page-header item-title">
@@ -53,55 +52,62 @@ JHtml::_('bootstrap.tooltip');
 <?php
 					else:
 ?>
-						<h3 class="page-header item-title">
-							<a href="<?php echo JRoute::_(ContentHelperRoute::getCategoryRoute($child->id));?>">
-								<?php echo $this->escape($child->title); ?>
-							</a>
-<?php
-							if($this->params->get('show_cat_num_articles', 1)):
-?>
-								<span class="badge badge-info tip hasTooltip" title="<?php echo JHtml::tooltipText('COM_CONTENT_NUM_ITEMS'); ?>">
-									<?php echo $child->getNumItems(true); ?>
-								</span>
-<?php
-							endif;
-						
-							if(count($child->getChildren()) > 0 && $this->maxLevel > 1):
-?>
-								<a href="#category-<?php echo $child->id;?>" data-toggle="collapse" data-toggle="button" class="btn btn-mini pull-right">
-									<span class="icon-plus"></span>
+							<div>
+								<a href="<?php echo JRoute::_(ContentHelperRoute::getCategoryRoute($child->id));?>">
+									<?php echo JHtml::_('content.prepare', $child->description, '', 'com_content.category'); ?>
 								</a>
-<?php
-							endif;
-?>
-						</h3>
-<?php
-					endif;
-
-					if($this->params->get('show_subcat_desc') == 1):
-						if($child->description):
-?>
-							<div class="category-desc">
-								<?php echo JHtml::_('content.prepare', $child->description, '', 'com_content.category'); ?>
 							</div>
+							<h3>
+								<a href="<?php echo JRoute::_(ContentHelperRoute::getCategoryRoute($child->id));?>">
+									<?php echo $this->escape($child->title); ?>
+								</a>
+							</h3>
 <?php
-						endif;
+							//if($this->params->get('show_cat_num_articles', 1)):
+?>
+								<!--<span class="badge badge-info tip hasTooltip" title="<?php //echo JHtml::tooltipText('COM_CONTENT_NUM_ITEMS'); ?>">
+									<?php //echo $child->getNumItems(true); ?>
+								</span>-->
+<?php
+							//endif;
+						
+							//if(count($child->getChildren()) > 0 && $this->maxLevel > 1):
+?>
+								<!--<a href="#category-<?php //echo $child->id;?>" data-toggle="collapse" data-toggle="button" class="btn btn-mini pull-right">
+									<span class="icon-plus"></span>
+								</a>-->
+<?php
+							//endif;
+?>
+<?php
 					endif;
 
-					if(count($child->getChildren()) > 0 && $this->maxLevel > 1):
+					//if($this->params->get('show_subcat_desc') == 1):
+						//if($child->description):
 ?>
-						<div class="collapse fade" id="category-<?php echo $child->id;?>">
+							<!--<div class="category-desc">
+								<?php //echo JHtml::_('content.prepare', $child->description, '', 'com_content.category'); ?>
+							</div>-->
+<?php
+						//endif;
+					//endif;
+
+					//if(count($child->getChildren()) > 0 && $this->maxLevel > 1):
+?>
+						<!--<div class="collapse fade" id="category-<?php //echo $child->id;?>">
 							<?php
+								/*
 								$this->children[$child->id] = $child->getChildren();
 								$this->category = $child;
 								$this->maxLevel--;
 								echo $this->loadTemplate('children');
 								$this->category = $child->getParent();
 								$this->maxLevel++;
+								*/
 							?>
-						</div>
+						</div>-->
 <?php
-					endif;
+					//endif;
 ?>
 				</div>
 <?php
