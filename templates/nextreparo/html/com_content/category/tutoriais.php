508ec5ec9402f396fbaf947aff04ca03d7861de0
Mariana Lino Ferreira <marianas2lino@gmail.com>
2016-04-11 11:43:34 -0300

    Pagina de tutorial. Listagem e exibicao de tutoriais - pagina da categoria

--- templates/nextreparo/html/com_content/category/tutoriais.php
+++ templates/nextreparo/html/com_content/category/tutoriais.php
@@ -19,12 +19,18 @@ JHtml::_('behavior.caption');
 	$page_var2 = $pieces2[0];
 ?>
 	<div class="blog<?php echo $this->pageclass_sfx; ?>" itemscope itemtype="http://schema.org/Blog">
+		<?php echo $this->category->description; ?>
+		<!--
+		TEXTO CABEÇALHO
 		<?php if ($this->params->get('show_page_heading')) : ?>
 			<div class="page-header">
 				<h1> <?php echo $this->escape($this->params->get('page_heading')); ?> </h1>
 			</div>
 		<?php endif; ?>
+		-->
 
+		<!--
+		TÍTULO CATEGORIA
 		<?php if ($this->params->get('show_category_title', 1) or $this->params->get('page_subheading')) : ?>
 			<h2>
 				<?php echo $this->escape($this->params->get('page_subheading')); ?>
@@ -33,27 +39,41 @@ JHtml::_('behavior.caption');
 				<?php endif; ?>
 			</h2>
 		<?php endif; ?>
+		-->
 
+		<!--
+		CATEGORIAS / TAGS
 		<?php if ($this->params->get('show_cat_tags', 1) && !empty($this->category->tags->itemTags)) : ?>
 			<?php $this->category->tagLayout = new JLayoutFile('joomla.content.tags'); ?>
 			<?php echo $this->category->tagLayout->render($this->category->tags->itemTags); ?>
 		<?php endif; ?>
-		<?php echo $this->category->description; ?>
+		-->
+
+		<!--
+		DESCRIÇÃO
 		<?php if ($this->params->get('show_description', 1) || $this->params->def('show_description_image', 1)) : ?>
 			<div class="category-desc clearfix">
-				
+				<?php if ($this->params->get('show_description_image') && $this->category->getParams()->get('image')) : ?>
+					<img src="<?php echo $this->category->getParams()->get('image'); ?>" alt="<?php echo htmlspecialchars($this->category->getParams()->get('image_alt')); ?>"/>
+				<?php endif; ?>
 				<?php if ($this->params->get('show_description') && $this->category->description) : ?>
 					<?php echo JHtml::_('content.prepare', $this->category->description, '', 'com_content.category'); ?>
 				<?php endif; ?>
 			</div>
 		<?php endif; ?>
+		-->
 
+		<!--
+		TEXTO 'SEM ARTIGOS'
 		<?php if (empty($this->lead_items) && empty($this->link_items) && empty($this->intro_items)) : ?>
 			<?php if ($this->params->get('show_no_articles', 1)) : ?>
 				<p><?php echo JText::_('COM_CONTENT_NO_ARTICLES'); ?></p>
 			<?php endif; ?>
 		<?php endif; ?>
+		-->
 
+		<!--
+		ARTIGOS PRINCIPAIS
 		<?php $leadingcount = 0; ?>
 		<?php if (!empty($this->lead_items)) : ?>
 			<div class="items-leading clearfix">
@@ -67,12 +87,19 @@ JHtml::_('behavior.caption');
 					<?php $leadingcount++; ?>
 				<?php endforeach; ?>
 			</div><!-- end items-leading -->
+		<!--
 		<?php endif; ?>
+		-->
 
+		<!--
+		ZERAGEM CONTADOR
 		<?php
 			$introcount = (count($this->intro_items));
 			$counter = 0;
 		?>
+		-->
+
+		<br class="clear" />
 		<!--
 		<div class="uk-grid-width-1-1 uk-grid-width-medium-1-3 uk-grid-small uk-grid uk-grid-match"  data-uk-grid-margin data-uk-grid-match="{target:'.uk-panel'}">
 		-->
@@ -101,11 +128,14 @@ JHtml::_('behavior.caption');
 			<?php endforeach; ?>
 		<?php endif; ?>
 
+		<!--
+		LINKS
 		<?php if (!empty($this->link_items)) : ?>
 			<div class="items-more">
 				<?php echo $this->loadTemplate('links'); ?>
 			</div>
 		<?php endif; ?>
+		-->
 
 	 	<?php if (($this->params->def('show_pagination', 1) == 1 || ($this->params->get('show_pagination') == 2)) && ($this->pagination->get('pages.total') > 1)) : ?>
 			<div class="uk-pagination">
