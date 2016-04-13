339f8f617abb0a26b1100d287aa7babedfe0a64b
Mariana Lino Ferreira <marianas2lino@gmail.com>
2016-04-13 17:07:50 -0300

    tela de exibicao do fórum

--- components/com_kunena/template/nts_ksliver/html/display.php
+++ components/com_kunena/template/nts_ksliver/html/display.php
@@ -10,19 +10,17 @@
 defined ( '_JEXEC' ) or die ();
 ?>
 <div id="Kunena" class="layout container-fluid">
-<?php
-if ($this->ktemplate->params->get('displayMenu', 1)) {
-	$this->displayMenu ();
-}
-$this->displayLoginBox ();
-//$this->displayBreadcrumb ();
+	<?php
+		if ($this->ktemplate->params->get('displayMenu', 1)) {
+			$this->displayMenu ();
+		}
+		$this->displayLoginBox ();
+		//$this->displayBreadcrumb ();
 
-// Display current view/layout
-$this->displayLayout();
+		// Display current view/layout
+		$this->displayLayout();
 
-$this->displayBreadcrumb ();
-$this->displayFooter ();
-?>
-
-
+		$this->displayBreadcrumb();
+		$this->displayFooter ();
+	?>
 </div>
