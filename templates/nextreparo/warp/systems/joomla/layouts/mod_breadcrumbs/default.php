0a0dba1388698bbe36bde2e595c5601448028b0f
Mariana Lino Ferreira <marianas2lino@gmail.com>
2016-04-14 11:19:18 -0300

    Ajuste arquivo breadcrumb para ajustes de itens mostrado e arrumacao na identacao do codigo

--- templates/nextreparo/warp/systems/joomla/layouts/mod_breadcrumbs/default.php
+++ templates/nextreparo/warp/systems/joomla/layouts/mod_breadcrumbs/default.php
@@ -10,32 +10,54 @@
 defined('_JEXEC') or die;
 
 ?>
-<ul class="uk-breadcrumb"><?php
+<ul class="uk-breadcrumb">
+	<?php
+		$url = $_SERVER["REQUEST_URI"];
+		$pieces = explode("/", $url);
+		$page_var = $pieces[2];
+		$pieces2 = explode("?", $page_var);
+		echo $page_var2 = $pieces2[0];
 
-	if (!$params->get('showLast', 1)) array_pop($list);
-
-	$count = count($list);
-
-	for ($i = 0; $i < $count; $i ++) {
-	
-		// clean subtitle from breadcrumb
-		if ($pos = strpos($list[$i]->name, '||')) {
-			$name = trim(substr($list[$i]->name, 0, $pos));
-		} else {
-			$name = $list[$i]->name;
+		if (!$params->get('showLast', 1))
+		{
+			array_pop($list);
 		}
-		
-		// mark-up last item as strong
-		if ($i < $count-1) {
-			if (!empty($list[$i]->link)) {
-				echo '<li><a href="'.$list[$i]->link.'">'.$name.'</a></li>';
-			} else {
-				echo '<li><span>'.$name.'</span></li>';
+
+		$count = count($list);
+
+		for ($i = 0; $i < $count; $i ++)
+		{
+			// clean subtitle from breadcrumb
+			if ($pos = strpos($list[$i]->name, '||'))
+			{
+				$name = trim(substr($list[$i]->name, 0, $pos));
 			}
-		} else {
-			echo '<li class="uk-active"><span>'.$name.'</span></li>';
+			else
+			{
+				$name = $list[$i]->name;
+			}
+		
+			// mark-up last item as strong
+			if($i < $count-1)
+			{
+
+				if(!empty($list[$i]->link))
+				{
+					echo '<li><a href="'.$list[$i]->link.'">'.$name.'</a></li>';
+				}
+				else
+				{
+					echo '<li><span>'.$name.'</span></li>';
+				}
+
+				if($i == 0 && @$page_var2 == "forum-reparo"){
+					echo '<li><a href="/reparosemlagrima/forum-reparo">Fórum</a></li>';
+				}
+			}
+			else
+			{
+				echo '<li class="uk-active"><span>'.$name.'</span></li>';
+			}
 		}
-
-	}
-
-?></ul>
+	?>
+</ul>
