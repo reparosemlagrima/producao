37e6e56e6765bd33c44aa795a1a1da716a3f5209
Mariana Lino Ferreira <marianas2lino@gmail.com>
2016-04-27 16:26:40 -0300

    Comentário das linhas das redes sociais não utilizadas

--- components/com_kunena/template/nts_ksliver/html/user/default_social.php
+++ components/com_kunena/template/nts_ksliver/html/user/default_social.php
@@ -16,25 +16,26 @@ if (!isset($this->showUnusedSocial)) $this->showUnusedSocial = false;
 <div class="kiconrow">
 	<?php echo $this->profile->socialButton('twitter', $this->showUnusedSocial); ?>
 	<?php echo $this->profile->socialButton('facebook', $this->showUnusedSocial); ?>
-	<?php echo $this->profile->socialButton('myspace', $this->showUnusedSocial); ?>
+	<?php //echo $this->profile->socialButton('myspace', $this->showUnusedSocial); ?>
 	<?php echo $this->profile->socialButton('linkedin', $this->showUnusedSocial); ?>
 	<?php echo $this->profile->socialButton('skype', $this->showUnusedSocial); ?>
+	<?php echo $this->profile->socialButton('gtalk', $this->showUnusedSocial); ?>
 </div>
-<div class="kiconrow">
-	<?php echo $this->profile->socialButton('delicious', $this->showUnusedSocial); ?>
-	<?php echo $this->profile->socialButton('friendfeed', $this->showUnusedSocial); ?>
-	<?php echo $this->profile->socialButton('digg', $this->showUnusedSocial); ?>
+<!--<div class="kiconrow">
+	<?php //echo $this->profile->socialButton('delicious', $this->showUnusedSocial); ?>
+	<?php //echo $this->profile->socialButton('friendfeed', $this->showUnusedSocial); ?>
+	<?php //echo $this->profile->socialButton('digg', $this->showUnusedSocial); ?>
 </div>
 <div class="clr"></div>
 <div class="kiconrow">
-	<?php echo $this->profile->socialButton('yim', $this->showUnusedSocial); ?>
-	<?php echo $this->profile->socialButton('aim', $this->showUnusedSocial); ?>
-	<?php echo $this->profile->socialButton('gtalk', $this->showUnusedSocial); ?>
-	<?php echo $this->profile->socialButton('icq', $this->showUnusedSocial); ?>
-	<?php echo $this->profile->socialButton('msn', $this->showUnusedSocial); ?>
+	<?php //echo $this->profile->socialButton('yim', $this->showUnusedSocial); ?>
+	<?php //echo $this->profile->socialButton('aim', $this->showUnusedSocial); ?>
+	
+	<?php //echo $this->profile->socialButton('icq', $this->showUnusedSocial); ?>
+	<?php //echo $this->profile->socialButton('msn', $this->showUnusedSocial); ?>
 </div>
 <div class="kiconrow">
-	<?php echo $this->profile->socialButton('blogspot', $this->showUnusedSocial); ?>
-	<?php echo $this->profile->socialButton('flickr', $this->showUnusedSocial); ?>
-	<?php echo $this->profile->socialButton('bebo', $this->showUnusedSocial); ?>
-</div>
+	<?php //echo $this->profile->socialButton('blogspot', $this->showUnusedSocial); ?>
+	<?php //echo $this->profile->socialButton('flickr', $this->showUnusedSocial); ?>
+	<?php //echo $this->profile->socialButton('bebo', $this->showUnusedSocial); ?>
+</div>-->
